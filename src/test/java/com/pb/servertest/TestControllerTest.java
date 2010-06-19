48ba2f9c1abef1171e0e03dc466de62056c01dd8
Patrick Bourke <pbourke@gmail.com>
2010-06-19 10:43:38 -0700

    Missed build.xml due to aggressive pattern in .gitignore. Added spring-test and junit dependencies. Remapped controller from test to app in web.xml. Added a simple JUnit test

---
+++ src/test/java/com/pb/servertest/TestControllerTest.java
@@ -0,0 +1,13 @@
+package com.pb.servertest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestControllerTest {
+
+    @Test
+    public void testFoo() {
+        new TestController();
+    }
+}
