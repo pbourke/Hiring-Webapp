040dde8364eff4c50958e4b4566a05e834b127f0
Patrick Bourke <pbourke@gmail.com>
2010-07-06 21:39:05 -0700

    Modified SkillsController to use ModelQueryHelper

--- src/main/java/com/pb/hiring/controllers/SkillsController.java
+++ src/main/java/com/pb/hiring/controllers/SkillsController.java
@@ -1,9 +1,6 @@
 package com.pb.hiring.controllers;
 
-import java.util.List;
-
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,20 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.pb.hiring.model.Skill;
+import com.pb.hiring.service.ModelQueryHelper;
 
 @Controller
 public class SkillsController {
     @Autowired
     private SessionFactory sessionFactory;
     
+    @Autowired
+    private ModelQueryHelper modelQueryHelper;
+    
     @Transactional
     @RequestMapping(method=RequestMethod.GET, value="/skills")
     public String listSkills(final ModelMap modelMap) {
-        final List<Skill> skills = sessionFactory.getCurrentSession()
-            .createCriteria( Skill.class )
-                .addOrder( Order.asc("title").ignoreCase() )
-                .list();
-        modelMap.put("skills", skills);
+        modelMap.put("skills", modelQueryHelper.allSkills().list());
         modelMap.put("newSkill", new Skill());
         return "skills/list";
     }
